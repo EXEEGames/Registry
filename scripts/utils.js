bf1ef25cf0b647c2495b183faa4e2130e13b5e3b
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2026-05-09 09:37:31 +0100

    Update Regsitry To Reflect new system changes

--- scripts/utils.js
+++
@@ -1,5 +0,0 @@
-export function generateGameId() {
-  const time = Math.floor(Date.now() / 1000);
-  const rand = Math.random().toString(36).substring(2, 7).toUpperCase();
-  return `EXEE-${time}-${rand}`;
-}
